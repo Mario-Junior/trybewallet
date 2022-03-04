ee8ac50baa2d6e426c381cd703492963cbf48ec3
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-03-04 16:24:25 -0300

    developing Login page - fields & entry button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,46 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <input
+          type="email"
+          name="email"
+          data-testid="email-input"
+          placeholder="email@domain.com"
+          value={ email }
+          onChange={ this.handleInputChange }
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          placeholder="insira sua senha"
+          value={ password }
+          onChange={ this.handleInputChange }
+        />
+        <button
+          type="button"
+          disabled
+        >
+          Entrar
+        </button>
+      </div>
+    )
   }
 }
 
