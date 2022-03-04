e8b8044f865fe8cd8a4401eca7737f965855ea0c
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-03-04 17:44:27 -0300

    developing Header component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <header>
+        <h4 data-testid="email-field">{user}</h4>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field"> BRL</span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({ user: state.user.email });
+
+Header.propTypes = {
+  user: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
